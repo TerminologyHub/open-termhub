ddccbbff4db150aaeefdd192bc4747da45ffda6f
Brian Carlsen <bcarlsen@westcoastinformatics.com>
2025-07-31 18:46:18 -0700

    fix

--- src/test/java/com/wci/termhub/fhir/r4/test/ValueSetLoadR4UnitTest.java
+++ src/test/java/com/wci/termhub/fhir/r4/test/ValueSetLoadR4UnitTest.java
@@ -9,17 +9,13 @@
  */
 package com.wci.termhub.fhir.r4.test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
@@ -32,10 +28,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.test.context.TestPropertySource;
 
 import com.wci.termhub.Application;
-import com.wci.termhub.model.HasId;
 import com.wci.termhub.service.EntityRepositoryService;
-import com.wci.termhub.util.ModelUtility;
-import com.wci.termhub.util.PropertyUtility;
 import com.wci.termhub.util.ValueSetLoaderUtil;
 
 /**
@@ -55,58 +48,6 @@ public class ValueSetLoadR4UnitTest {
   /** List of FHIR Code System files to load. */
   private static final List<String> VALUE_SET_FILES =
       List.of("ValueSet-snomedct_us-model-nlm-20240301-r4.json");
-
-  /**
-   * Setup - load the FHIR Code System files.
-   *
-   * @throws Exception the exception
-   */
-  @BeforeAll
-  public void setup() throws Exception {
-    // Use File instead of Path to avoid colon issues in directory paths
-    final String indexDirPath =
-        PropertyUtility.getProperties().getProperty("lucene.index.directory");
-
-    // Delete all indexes for a fresh start
-    LOGGER.info("Deleting existing indexes from directory: {}", indexDirPath);
-    final File indexDir = new File(indexDirPath);
-    LOGGER.info("Does this exist? {}", indexDir.exists());
-
-    if (indexDir.exists()) {
-      FileUtils.deleteDirectory(indexDir);
-    }
-
-    final List<Class<? extends HasId>> indexedObjects = ModelUtility.getIndexedObjects();
-    for (final Class<? extends HasId> clazz : indexedObjects) {
-      searchService.deleteIndex(clazz);
-      searchService.createIndex(clazz);
-    }
-
-    // Load each code system by reading directly from the classpath
-    for (final String valueSetFile : VALUE_SET_FILES) {
-      try {
-        // Read file from classpath directly using Spring's resource mechanism
-        final Resource resource = new ClassPathResource("data/" + valueSetFile,
-            ValueSetLoadR4UnitTest.class.getClassLoader());
-
-        if (!resource.exists()) {
-          throw new FileNotFoundException("Could not find resource: data/" + valueSetFile);
-        }
-
-        final String fileContent =
-            FileUtils.readFileToString(resource.getFile(), StandardCharsets.UTF_8);
-
-        LOGGER.info("Loading value sets from classpath resource: data/{}", valueSetFile);
-        assertNotNull(ValueSetLoaderUtil.loadSubset(searchService, fileContent, false));
-
-      } catch (final Exception e) {
-        LOGGER.error("Error loading value set file: {}", valueSetFile, e);
-        throw e;
-      }
-    }
-
-    LOGGER.info("Finished loading value sets");
-  }
 
   /**
    * Test reload value set.
