549575469add84c318a7f279a55b295dcfddc8cf
= <=>
2025-09-03 09:25:03 -0700

    OP-1435: Fix sort on name field

--- src/test/java/com/wci/termhub/util/test/IndexUtilityUnitTest.java
+++ src/test/java/com/wci/termhub/util/test/IndexUtilityUnitTest.java
@@ -11,6 +11,7 @@ package com.wci.termhub.util.test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
@@ -25,9 +26,11 @@ import org.apache.lucene.search.Sort;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 
 import com.wci.termhub.model.SearchParameters;
+import com.wci.termhub.model.Terminology;
 import com.wci.termhub.util.IndexUtility;
 
 /**
@@ -45,23 +48,23 @@ public class IndexUtilityUnitTest {
   private static class TestModel {
 
     /** The text field. */
-    @org.springframework.data.elasticsearch.annotations.Field(type = FieldType.Text)
+    @Field(type = FieldType.Text)
     private final String textField;
 
     /** The keyword field. */
-    @org.springframework.data.elasticsearch.annotations.Field(type = FieldType.Keyword)
+    @Field(type = FieldType.Keyword)
     private final String keywordField;
 
     /** The date field. */
-    @org.springframework.data.elasticsearch.annotations.Field(type = FieldType.Date)
+    @Field(type = FieldType.Date)
     private final Date dateField;
 
     /** The int field. */
-    @org.springframework.data.elasticsearch.annotations.Field(type = FieldType.Integer)
+    @Field(type = FieldType.Integer)
     private final Integer intField;
 
     /** The string list. */
-    @org.springframework.data.elasticsearch.annotations.Field(type = FieldType.Object)
+    @Field(type = FieldType.Object)
     private final List<String> stringList;
 
     /**
@@ -138,5 +141,193 @@ public class IndexUtilityUnitTest {
     assertNotNull(sort);
     assertEquals(1, sort.getSort().length);
     assertEquals("textField", sort.getSort()[0].getField());
+  }
+
+  /**
+   * Test field mapping - .keyword suffix mapping.
+   *
+   * @throws Exception the exception
+   */
+  @Test
+  public void testKeywordFieldMapping() throws Exception {
+    final SearchParameters params = new SearchParameters();
+    params.setSort(Arrays.asList("name.keyword"));
+    params.setAscending(true);
+
+    final Sort sort = IndexUtility.getSortOrder(params, Terminology.class);
+
+    assertNotNull(sort);
+    assertEquals(1, sort.getSort().length);
+    assertEquals("name.keyword", sort.getSort()[0].getField());
+  }
+
+  /**
+   * Test field mapping - .ngram suffix mapping.
+   *
+   * @throws Exception the exception
+   */
+  @Test
+  public void testNgramFieldMapping() throws Exception {
+    final SearchParameters params = new SearchParameters();
+    params.setSort(Arrays.asList("name.ngram"));
+    params.setAscending(true);
+
+    final Sort sort = IndexUtility.getSortOrder(params, Terminology.class);
+
+    assertNotNull(sort);
+    assertEquals(1, sort.getSort().length);
+    assertEquals("name.ngram", sort.getSort()[0].getField());
+  }
+
+  /**
+   * Test field mapping - nested field with .keyword suffix. Note: This test
+   * will fail because Terminology class doesn't have 'to' field. This is
+   * expected behavior - the test validates error handling.
+   */
+  @Test
+  public void testNestedKeywordFieldMapping() {
+    final SearchParameters params = new SearchParameters();
+    params.setSort(Arrays.asList("to.name.keyword"));
+    params.setAscending(true);
+
+    // This should throw NoSuchFieldException because 'to' field doesn't exist
+    // in Terminology
+    assertThrows(Exception.class, () -> {
+      IndexUtility.getSortOrder(params, Terminology.class);
+    });
+  }
+
+  /**
+   * Test field mapping - multiple field mappings.
+   *
+   * @throws Exception the exception
+   */
+  @Test
+  public void testMultipleFieldMappings() throws Exception {
+    final SearchParameters params = new SearchParameters();
+    params.setSort(Arrays.asList("name.keyword", "abbreviation", "version.keyword"));
+    params.setAscending(true);
+
+    final Sort sort = IndexUtility.getSortOrder(params, Terminology.class);
+
+    assertNotNull(sort);
+    assertEquals(3, sort.getSort().length);
+    assertEquals("name.keyword", sort.getSort()[0].getField());
+    assertEquals("abbreviation", sort.getSort()[1].getField());
+    assertEquals("version.keyword", sort.getSort()[2].getField());
+  }
+
+  /**
+   * Test field mapping - mixed .keyword and .ngram suffixes.
+   *
+   * @throws Exception the exception
+   */
+  @Test
+  public void testMixedSuffixMappings() throws Exception {
+    final SearchParameters params = new SearchParameters();
+    params.setSort(Arrays.asList("name.keyword", "name.ngram", "abbreviation"));
+    params.setAscending(true);
+
+    final Sort sort = IndexUtility.getSortOrder(params, Terminology.class);
+
+    assertNotNull(sort);
+    assertEquals(3, sort.getSort().length);
+    assertEquals("name.keyword", sort.getSort()[0].getField());
+    assertEquals("name.ngram", sort.getSort()[1].getField());
+    assertEquals("abbreviation", sort.getSort()[2].getField());
+  }
+
+  /**
+   * Test field mapping - complex nested field with .keyword. Note: This test
+   * will fail because Terminology class doesn't have 'concept' field. This is
+   * expected behavior - the test validates error handling.
+   */
+  @Test
+  public void testComplexNestedFieldMapping() {
+    final SearchParameters params = new SearchParameters();
+    params.setSort(Arrays.asList("concept.name.keyword"));
+    params.setAscending(true);
+
+    // This should throw NoSuchFieldException because 'concept' field doesn't
+    // exist in Terminology
+    assertThrows(Exception.class, () -> {
+      IndexUtility.getSortOrder(params, Terminology.class);
+    });
+  }
+
+  /**
+   * Test field mapping - field that doesn't exist should throw exception.
+   */
+  @Test
+  public void testFieldMappingNonExistentField() {
+    final SearchParameters params = new SearchParameters();
+    params.setSort(Arrays.asList("nonExistentField.keyword"));
+    params.setAscending(true);
+
+    try {
+      IndexUtility.getSortOrder(params, Terminology.class);
+      // If we get here, the test should fail
+      assertTrue(false, "Expected NoSuchFieldException for non-existent field");
+    } catch (final NoSuchFieldException e) {
+      // Expected exception
+      assertTrue(e.getMessage().contains("nonExistentField"));
+      assertTrue(e.getMessage().contains("Terminology"));
+    }
+  }
+
+  /**
+   * Test field mapping - regular Java field (no suffix) still works.
+   *
+   * @throws Exception the exception
+   */
+  @Test
+  public void testRegularJavaFieldStillWorks() throws Exception {
+    final SearchParameters params = new SearchParameters();
+    params.setSort(Arrays.asList("abbreviation"));
+    params.setAscending(true);
+
+    final Sort sort = IndexUtility.getSortOrder(params, Terminology.class);
+
+    assertNotNull(sort);
+    assertEquals(1, sort.getSort().length);
+    assertEquals("abbreviation", sort.getSort()[0].getField());
+  }
+
+  /**
+   * Test field mapping - field from superclass works.
+   *
+   * @throws Exception the exception
+   */
+  @Test
+  public void testFieldMappingFromSuperclass() throws Exception {
+    final SearchParameters params = new SearchParameters();
+    params.setSort(Arrays.asList("name.keyword"));
+    params.setAscending(true);
+
+    // Terminology extends TerminologyRef, and 'name' field is in TerminologyRef
+    final Sort sort = IndexUtility.getSortOrder(params, Terminology.class);
+
+    assertNotNull(sort);
+    assertEquals(1, sort.getSort().length);
+    assertEquals("name.keyword", sort.getSort()[0].getField());
+  }
+
+  /**
+   * Test field mapping - descending sort order.
+   *
+   * @throws Exception the exception
+   */
+  @Test
+  public void testFieldMappingDescendingSort() throws Exception {
+    final SearchParameters params = new SearchParameters();
+    params.setSort(Arrays.asList("name.keyword"));
+    params.setAscending(false);
+
+    final Sort sort = IndexUtility.getSortOrder(params, Terminology.class);
+
+    assertNotNull(sort);
+    assertEquals(1, sort.getSort().length);
+    assertEquals("name.keyword", sort.getSort()[0].getField());
+    assertTrue(sort.getSort()[0].getReverse(), "Sort should be descending");
   }
 }
