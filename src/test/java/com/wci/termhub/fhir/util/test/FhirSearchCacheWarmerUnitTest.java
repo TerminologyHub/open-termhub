7b4226a7762d6fb34e994ff7e06b2c44662c4050
Brian Carlsen <bcarlsen@westcoastinformatics.com>
2026-09-23 11:59:20 -0700

    Update UI

--- src/test/java/com/wci/termhub/fhir/util/test/FhirSearchCacheWarmerUnitTest.java
+++ src/test/java/com/wci/termhub/fhir/util/test/FhirSearchCacheWarmerUnitTest.java
@@ -69,6 +69,7 @@ public class FhirSearchCacheWarmerUnitTest {
   /**
    * Sets up mocks.
    */
+  @SuppressWarnings("resource")
   @BeforeEach
   public void setUp() {
     MockitoAnnotations.openMocks(this);
